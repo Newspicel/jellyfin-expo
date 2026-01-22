863bd6203ff152d6d049344c6b03784cc434a7c7
Julian Haag <me@jhaag.me>
2026-01-22 04:07:56 +0100

    series view

--- app/(tabs)/library/[libraryId].tsx
+++ app/(tabs)/library/[libraryId].tsx
@@ -80,7 +80,7 @@ export default function LibraryItemsScreen() {
         parentId: libraryId,
         sortBy: ['SortName'],
         sortOrder: ['Ascending'],
-        recursive: true,
+        recursive: false,
         enableImages: true,
         enableImageTypes: ['Primary', 'Backdrop', 'Thumb'],
         imageTypeLimit: 1,
